a0e60e4482658076ed5c53b57bc91655299b5c2d
GCCISAdmin <gccisadmin@gol2670-01.main.ad.rit.edu>
2021-11-20 20:23:48 -0500

    created project

---
+++ GolisanoChat/GolisanoChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  GolisanoChatApp.swift
+//  GolisanoChat
+//
+//  Created by GCCISAdmin on 11/20/21.
+//
+
+import SwiftUI
+
+@main
+struct GolisanoChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
