d6f5ba3a7da0bba58c7aeb55ced4b40894d24979
GCCISAdmin <gccisadmin@gol2670-01.main.ad.rit.edu>
2021-11-20 21:23:36 -0500

    made controllers

---
+++ GolisanoChat/Controllers/ChatController.swift
@@ -0,0 +1,17 @@
+//
+//  ChatController.swift
+//  ChatController
+//
+//  Created by GCCISAdmin on 11/20/21.
+//
+
+import Foundation
+import UIKit
+
+class ChatController: UIViewController{
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
